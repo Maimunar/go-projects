fc6184cf0a4e704a7cadd90d15009cb2855785b1
Aleksandar Ivanov <metalcho17@gmail.com>
2024-08-19 12:34:29 +0200

    delete unused code

--- calculator-api/internal/handlers/add.go
+++ calculator-api/internal/handlers/add.go
@@ -29,21 +29,3 @@ func AddHandler(r *database.SQLiteRepository) gin.HandlerFunc {
 		})
 	}
 }
-
-// func AddHandler(c *gin.Context) {
-// 	var body api.TwoNumbersBody
-//
-// 	err := c.BindJSON(&body)
-// 	if err != nil {
-// 		c.JSON(400, gin.H{
-// 			"message": "Invalid input",
-// 		})
-// 		return
-// 	}
-//
-// 	result := body.Number1 + body.Number2
-//
-// 	c.JSON(200, gin.H{
-// 		"result": result,
-// 	})
-// }
